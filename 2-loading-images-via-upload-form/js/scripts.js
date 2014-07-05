40c40b3b58b89c986ab0751c4d13213ea59a1ad8
Patrick Catanzariti <thatpatrickguy@gmail.com>
2014-07-05 18:42:06 +1000

    Renamed a few folders

---
+++ 2-loading-images-via-upload-form/js/scripts.js
@@ -0,0 +1,79 @@
+var canvas = document.getElementById('our-canvas'), // Our canvas element
+    ourData = document.getElementById('our-data'), // Our <div> which starts empty
+    context = canvas.getContext('2d'),
+    uploadedFile = document.getElementById('uploaded-file'); // Our file uploader option
+
+window.addEventListener("load", initImageLoader, true);
+
+function initImageLoader() {
+  uploadedFile.addEventListener("change", handleManualUploadedFiles, false);
+  
+  function handleManualUploadedFiles(response) {
+    var _file = response.target.files[0];
+    handleFile(_file);
+  }
+}
+
+function handleFile(file) {
+    var imageType = /image.*/;
+
+    if (!file.type.match(imageType)) {
+        return false;
+    }
+
+    if (file.type.match(imageType)) {
+        var img = document.createElement('img'),
+            reader = new FileReader();
+
+        reader.onloadend = function(event) {
+            var _tempImageStore = new Image(),
+                _image = null;
+      
+            // Set up our onload function - it won't run this code till we've loaded an image into our _tempImageStore.
+            _tempImageStore.onload = function(ev) {
+              // Gets the image width and height, sets the canvas size to match it.
+              canvas.height = ev.target.height;
+              canvas.width = ev.target.width;
+
+              // Draw that image into our canvas element
+              context.drawImage(ev.target, 0, 0);
+              
+              // We read in the data in our canvas element now, storing it as an array of data in the form of a one dimensional array of RGBARGBA... etc.
+              _image = context.getImageData(0, 0, ev.target.width, ev.target.height);
+              
+              var _generatedImageArray = new Array(), // We'll separate out each RGBA pixel of data into a more understandable array
+                  _textArray = ''; // This is to make it easy for us to read our array data in this demo
+              
+              for (y = 0; y < canvas.height; y++) {
+                _generatedImageArray[y] = new Array();
+                _textArray += '[';
+
+                for (x = 0; x < canvas.width; x++) {
+                  // https://developer.mozilla.org/En/HTML/Canvas/Pixel_manipulation_with_canvas
+                  var _red = _image.data[((y * (_image.width * 4)) + (x * 4))],
+                      _green = _image.data[((y * (_image.width * 4)) + (x * 4)) + 1],
+                      _blue = _image.data[((y * (_image.width * 4)) + (x * 4)) + 2],
+                      _alpha = _image.data[((y * (_image.width * 4)) + (x * 4)) + 3];
+                  
+                  _generatedImageArray[y][x] = [_red,_green,_blue,_alpha];
+                  _textArray += '['+_red+','+_green+','+_blue+','+_alpha+']';
+
+                  if (x != canvas.width - 1) _textArray += ',';
+                }
+
+                _textArray += ']';
+
+                if (y != canvas.height - 1) _textArray += ',';
+              }
+              
+              ourData.innerHTML = _textArray;
+            }
+          
+          _tempImageStore.src = event.target.result; // Put the loaded image into our new image object, triggering the above onload function.
+        }
+
+        reader.readAsDataURL(file); // Read our image data and then it'll run our onloadend function above
+    }
+
+    return true;
+}
